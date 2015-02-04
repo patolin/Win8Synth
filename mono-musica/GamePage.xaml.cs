0e72fbb04adb81169354a5532fc7aeaf71a0511a
Patricio Reinoso M. <patricio.reinoso.mendoza@gmail.com>
2015-02-04 23:49:53 +0100

    Carga de archivos
    Version final

---
+++ mono-musica/GamePage.xaml.cs
@@ -0,0 +1,261 @@
+﻿using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Input;
+
+using MonoGame.Framework;
+using Windows.ApplicationModel.Activation;
+using SharpDX;
+using SharpDX.XAudio2;
+using SharpDX.Multimedia;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace mono_musica
+{
+    /// <summary>
+    /// The root page used to display the game.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly Game1 _game;
+        Dictionary<uint, sonido> dictSonidos = new Dictionary<uint,sonido>();
+
+        
+        XAudio2 xaudio2;
+        MasteringVoice masteringVoice;
+
+        private sonido.onda syntActual= sonido.onda.sinusoidal ;
+        private sonido.onda envoActual = sonido.onda.ninguno;
+
+
+        static int envolventeMax = 60;
+        private int envolventeFreq = 0;
+
+        public GamePage(LaunchActivatedEventArgs args)
+        {
+            this.InitializeComponent();
+            
+
+            // Create the game.
+            _game = XamlGame<Game1>.Create(args, Window.Current.CoreWindow, this);
+
+            xaudio2 = new XAudio2();
+            masteringVoice = new MasteringVoice(xaudio2);
+
+            textbox1.Text = "";
+
+            perilla1.RenderTransform = new CompositeTransform();
+            perilla2.RenderTransform = new CompositeTransform();
+            perilla3.RenderTransform = new CompositeTransform();
+
+        }
+
+       
+
+        private void pointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+            PointerPoint ptrPt = e.GetCurrentPoint(img);
+            uint pointerId = ptrPt.PointerId;
+
+            textbox1.Text  += "P id:"+ptrPt.PointerId+"\tX:\t" + ptrPt.Position.X +"\tY:\t" + ptrPt.Position.Y+"\n";
+
+            int frecuencia = (int)(ptrPt.Position.X * 2048.0f / 640.0f);
+            float vol = (float)(ptrPt.Position.Y / 1280.0f);
+            if (!dictSonidos.ContainsKey(ptrPt.PointerId))
+            {
+                dictSonidos.Add(ptrPt.PointerId, new sonido(vol, frecuencia, ptrPt.PointerId, xaudio2, syntActual, envoActual, envolventeFreq));
+              
+                dictSonidos[pointerId].play();
+            }
+            else
+            {
+                //dictSonidos[ptrPt.PointerId]= new sonido(vol,frecuencia, ptrPt.PointerId, xaudio2);
+                dictSonidos[pointerId].play();
+            }
+
+            e.Handled = true;
+
+            
+            
+
+        }
+
+        private void pointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+            PointerPoint ptrPt = e.GetCurrentPoint(img);
+            uint pointerId = ptrPt.PointerId;
+            
+            int frecuenciaN = (int)(ptrPt.Position.X * 2048.0f / 640.0f);
+            float volN = (float)(ptrPt.Position.Y / 1280.0f);
+            if (dictSonidos.ContainsKey(pointerId))
+            {
+                dictSonidos[pointerId].cambioFrec(frecuenciaN, envolventeFreq);
+                dictSonidos[pointerId].cambioVol(volN);
+                dictSonidos[pointerId].play();
+            }
+            /*
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+            PointerPoint ptrPt = e.GetCurrentPoint(img);
+            textbox1.Text += "M:id:" + ptrPt.PointerId + "\tX:\t" + ptrPt.Position.X + "\tY:\t" + ptrPt.Position.Y + "\n";
+             * */
+            e.Handled = true;
+        }
+
+
+        private void pointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+            PointerPoint ptrPt = e.GetCurrentPoint(img);
+            uint pointerId = ptrPt.PointerId;
+            if (dictSonidos.ContainsKey(pointerId))
+            {
+                dictSonidos.Remove(pointerId);
+            }
+            e.Handled = true;
+        }
+
+
+        private void btnOndaPress(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement boton = (FrameworkElement)e.OriginalSource;
+            switch (boton.Name)
+            {
+                case "swOnda1":
+                    {
+                        swOnda1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        swOnda2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swOnda3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        syntActual = sonido.onda.sinusoidal;
+
+                        break;
+                    }
+                case "swOnda2":
+                    {
+                        swOnda1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swOnda2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        swOnda3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        syntActual = sonido.onda.triangular;
+                        break;
+                    }
+                case "swOnda3":
+                    {
+                        swOnda1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swOnda2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swOnda3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        syntActual = sonido.onda.cuadrada;
+                        break;
+                    }
+            }
+            e.Handled = true;
+        }
+
+        private void btnEnvolventePress(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement boton = (FrameworkElement)e.OriginalSource;
+            switch (boton.Name)
+            {
+                case "swEnv1":
+                    {
+                        swEnv1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        swEnv2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv4.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        envoActual = sonido.onda.sinusoidal;
+
+                        var transform = perilla1.RenderTransform  as CompositeTransform;
+                        envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+                        textbox1.Text += "P1: " + envolventeFreq+"\n";
+                        
+                        break;
+                    }
+                case "swEnv2":
+                    {
+                        swEnv1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        swEnv3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv4.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        envoActual = sonido.onda.triangular;
+                        var transform = perilla2.RenderTransform as CompositeTransform;
+                        envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+                        textbox1.Text += "P2: " + envolventeFreq + "\n";
+                        break;
+                    }
+                case "swEnv3":
+                    {
+                        swEnv1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        swEnv4.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        envoActual = sonido.onda.cuadrada;
+                        var transform = perilla3.RenderTransform as CompositeTransform;
+                        envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+                        textbox1.Text += "P3: " + envolventeFreq + "\n";
+                        break;
+                    }
+                case "swEnv4":
+                    {
+                        swEnv1.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv2.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv3.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_off.jpg", UriKind.Absolute) };
+                        swEnv4.Source = new BitmapImage() { UriSource = new Uri("ms-appx:///Assets/sw_on.jpg", UriKind.Absolute) };
+                        envoActual = sonido.onda.ninguno;
+                        break;
+                    }
+            }
+
+            e.Handled = true;
+
+
+        }
+
+        private void rotacionPerilla(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            FrameworkElement source = e.OriginalSource as FrameworkElement;
+            var transform = source.RenderTransform as CompositeTransform;
+            transform.Rotation += e.Delta.Rotation;
+
+            if (transform.Rotation < 0) { transform.Rotation = 0; }
+            if (transform.Rotation >180 ) { transform.Rotation = 180; }
+            source.RenderTransform = transform;
+
+
+            if (source.Name == "perilla1" && envoActual == sonido.onda.sinusoidal)
+            {
+                envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+            }
+
+            if (source.Name == "perilla2" && envoActual == sonido.onda.triangular)
+            {
+                envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+            }
+
+            if (source.Name == "perilla3" && envoActual == sonido.onda.cuadrada)
+            {
+                envolventeFreq = (int)(transform.Rotation * envolventeMax / 180);
+            }
+
+
+            e.Handled = true;
+            
+            
+        }
+
+
+
+        
+ 
+    }
+}
---
+++ mono-musica/sonido.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MonoGame.Framework;
+using Windows.ApplicationModel.Activation;
+using SharpDX;
+using SharpDX.XAudio2;
+using SharpDX.Multimedia;
+using System;
+using System.IO;
+using System.Collections;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace mono_musica
+{
+    class sonido
+    {
+        public enum onda
+        {
+            sinusoidal,
+            triangular,
+            cuadrada,
+            ninguno
+        }
+        private int frec;
+
+        private float vol = 1.0f;
+        private uint pointerId;
+        private SourceVoice sourcevoice;
+        private static int sampleRate = 44100;
+        // buffer de 1s
+        private short[] BufferSamples = new short[sampleRate * 2 * 1];
+        //private static short[] BufferSamples = new short[2048];
+
+        private onda tipoOnda = onda.sinusoidal;
+        private onda tipoEnvolvente = onda.ninguno;
+
+        public sonido(float volumen, int frecuencia, uint puntero, XAudio2 xaudio, onda ondaOriginal, onda envoOriginal, int frecuenciaEnvolvente)
+        {
+            frec = frecuencia;
+            vol = volumen / 2.0f;
+            tipoOnda = ondaOriginal;
+            tipoEnvolvente = envoOriginal;
+
+            llenaBuffer(BufferSamples, frec, frecuenciaEnvolvente);
+            pointerId = puntero;
+            var waveFormat = new WaveFormat();
+
+
+            sourcevoice = new SourceVoice(xaudio, waveFormat, true);
+            /*
+            FilterParameters filtro = new FilterParameters();
+            filtro.Frequency = 0.3f;
+            filtro.Type = 0;
+            filtro.OneOverQ = 1.0f;
+            sourcevoice.SetFilterParameters(filtro);
+            */
+        }
+
+        public void llenaBuffer(short[] buffer, int frecuencia, int frecuenciaEnvolvente)
+        {
+            double totalTime;
+
+            // generamos el buffer de la envoltura
+            totalTime = 0;
+            double[] bufferE = new double[buffer.Length];
+            for (int i = 0; i < buffer.Length - 1; i += 2)
+            {
+                double time = (double)totalTime / (double)sampleRate;
+                double currentSample = 0;
+                if (this.tipoEnvolvente == onda.sinusoidal)
+                {
+                    currentSample = (double)(Math.Sin(2 * Math.PI * frecuenciaEnvolvente * time));
+                }
+
+                if (this.tipoEnvolvente == onda.triangular)
+                {
+                    currentSample = (double)(2 * (time * frecuenciaEnvolvente - Math.Floor(time * frecuenciaEnvolvente + 0.5)));
+                }
+
+                if (this.tipoEnvolvente == onda.cuadrada)
+                {
+                    currentSample = (double)(Math.Sin(2 * Math.PI * frecuenciaEnvolvente * time) >= 0 ? 1 : 0);
+
+                }
+
+                if (this.tipoEnvolvente == onda.ninguno)
+                {
+                    currentSample = 1;
+                }
+
+
+                bufferE[i] = currentSample;
+                bufferE[i + 1] = currentSample;
+
+                totalTime++;
+            }
+
+            // generamos la onda
+            totalTime = 0;
+            for (int i = 0; i < buffer.Length - 1; i += 2)
+            {
+                double time = (double)totalTime / (double)sampleRate;
+                short currentSample = 0;
+
+                if (this.tipoOnda == onda.sinusoidal)
+                {
+                    currentSample = (short)(Math.Sin(2 * Math.PI * frecuencia * time) * (double)short.MaxValue);
+                }
+
+                if (this.tipoOnda == onda.triangular)
+                {
+                    currentSample = (short)(2 * (time * frecuencia - Math.Floor(time * frecuencia + 0.5)) * (double)short.MaxValue);
+                }
+
+                if (this.tipoOnda == onda.cuadrada)
+                {
+                    currentSample = (short)(Math.Sin(2 * Math.PI * frecuencia * time) >= 0 ? short.MaxValue : short.MinValue);
+
+                }
+
+
+                buffer[i] = (short)(currentSample * bufferE[i]);
+                buffer[i + 1] = (short)(currentSample * bufferE[i + 1]);
+
+                totalTime++;
+            }
+        }
+
+        public void play()
+        {
+            var dataStream = DataStream.Create(BufferSamples, true, true);
+            AudioBuffer buffer = new AudioBuffer
+            {
+                /*LoopCount = AudioBuffer.LoopInfinite,*/
+                Stream = dataStream,
+                AudioBytes = (int)dataStream.Length,
+                Flags = BufferFlags.EndOfStream
+            };
+
+
+            sourcevoice.SubmitSourceBuffer(buffer, null);
+            sourcevoice.SetVolume(vol);
+
+
+
+
+            sourcevoice.Start();
+
+
+
+        }
+
+        public void cambioFrec(int frecuenciaNueva, int frecuenciaEnvolvente)
+        {
+            if (sourcevoice != null)
+            {
+
+                sourcevoice.SetVolume(0.0f);
+                sourcevoice.FlushSourceBuffers();
+
+                //sourcevoice.SetFrequencyRatio(frecuenciaNueva / 2048);
+
+
+            }
+            sourcevoice.SetVolume(vol);
+            llenaBuffer(BufferSamples, frecuenciaNueva, frecuenciaEnvolvente);
+            sourcevoice.Start();
+
+        }
+
+        public void cambioOnda(onda ondaNueva)
+        {
+            this.tipoOnda = ondaNueva;
+        }
+
+        public void cambioVol(float volumen)
+        {
+            if (sourcevoice != null)
+            {
+                vol = volumen / 2.0f;
+                sourcevoice.SetVolume(vol);
+
+            }
+        }
+
+
+
+    }
+}
